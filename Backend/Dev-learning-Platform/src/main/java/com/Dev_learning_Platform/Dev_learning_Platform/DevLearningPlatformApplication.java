1abc432f1f1f9f0627027693bc2e3d8a990920db
Juan-Valenzuela3 <juan_svalenzuela@soy.sena.edu.co>
2025-08-25 20:15:53 -0500

    First commit

---
+++ Backend/Dev-learning-Platform/src/main/java/com/Dev_learning_Platform/Dev_learning_Platform/DevLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Dev_learning_Platform.Dev_learning_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLearningPlatformApplication.class, args);
+	}
+
+}
