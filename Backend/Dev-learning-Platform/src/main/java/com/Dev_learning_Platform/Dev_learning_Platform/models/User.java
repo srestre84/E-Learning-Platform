86ecfe5d944e6952bde3cdc84978a3edc4aebcd1
Juan-Valenzuela3 <juan_svalenzuela@soy.sena.edu.co>
2025-08-27 12:32:34 -0500

    Config files proyect

---
+++ Backend/Dev-learning-Platform/src/main/java/com/Dev_learning_Platform/Dev_learning_Platform/models/User.java
@@ -0,0 +1,9 @@
+package com.Dev_learning_Platform.Dev_learning_Platform.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+}
