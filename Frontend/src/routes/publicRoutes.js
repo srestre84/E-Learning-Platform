6bdfeac90dda0c41f875704b99aa303e0559316d
Alejandro <alejodev2022@gmail.com>
2025-08-27 23:45:03 -0500

    fix: agregar espacio en blanco al inicio del archivo publicRoutes.js para mejorar la legibilidad

--- Frontend/src/routes/publicRoutes.js
+++ Frontend/src/routes/publicRoutes.js
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+
 const Home = lazy(() => import("@/pages/Home"))
 const Login = lazy(() => import("@/pages/Login"))
 const Register = lazy(() => import("@/pages/Register"))
