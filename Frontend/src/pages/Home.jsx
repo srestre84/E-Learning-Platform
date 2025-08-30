e2441302e1cd71b0f7b68218bc93524ba761b926
Alejandro <alejodev2022@gmail.com>
2025-08-30 14:35:46 -0500

    feat: reestructurar la página de inicio para incluir nuevos componentes como HeroSection, CursoDisponible, Price, InstructoresCard, TestimonioCard y CtaButton

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
+import CtaButton from "@/components/Landing/CTAButton";
+import CursoDisponible from "@/components/Landing/CursoCard";
+import HeroSection from "@/components/Landing/HeroSection";
+import InstuctoresCard from "@/components/Landing/InstructoresCard";
+import Price from "@/components/Landing/Price";
+import TestimonioCard from "@/components/Landing/testimonioCard";
+
+
 export default function Home() {
   return (
-    <section className=" bg-hero-gradient py-20 text-center">
-      <div className="bg-" >
+    <>
+     <HeroSection/>
+     <CursoDisponible/>
+     <Price/>
+     <InstuctoresCard/>
+     <TestimonioCard/>
+     <CtaButton/>
+    </>
 
-      </div>
 
-    </section>
+
   );
 }
