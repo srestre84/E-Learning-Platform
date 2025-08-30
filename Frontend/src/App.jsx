663066486d1b7ec395b1c817c9f98872c65b65ac
Alejandro <alejodev2022@gmail.com>
2025-08-30 10:14:09 -0500

    feat: implementar enrutamiento en el componente App utilizando RouterProvider y Suspense para manejo de carga

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import Header from '@/components/common/Header'
+
+import { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/routes";
 function App() {
-
-
   return (
-
-
-    <Header/>
-
-  )
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          Cargando...
+        </div>
+      }>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
