c8109d5e53efbeaff2f1274506149c10445ca552
Alejandro <alejodev2022@gmail.com>
2025-08-30 10:14:28 -0500

    refactor: actualizar la importación de React y el método de renderizado para utilizar StrictMode y mejorar la estructura del componente principal

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <App/>
+  </React.StrictMode>,
 )
