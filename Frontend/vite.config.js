e508ddbd1b88d511f7b5d407379d46c679434265
Alejandro <alejodev2022@gmail.com>
2025-08-30 10:13:57 -0500

    chore: ajustar el formato de la configuración de Vite para mejorar la legibilidad del código

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+       '@': path.resolve(__dirname, './src')
     }
   }
 })
