f3838f5a1e30ec610a2bb966c88051d12aa444cc
Alejandro <alejodev2022@gmail.com>
2025-09-01 10:23:45 -0500

    feat: añadir el componente Dashboard en AppLayout para mejorar la navegación y la estructura de la aplicación

--- Frontend/src/components/layout/AppLayout.jsx
+++ Frontend/src/components/layout/AppLayout.jsx
@@ -1,9 +1,11 @@
 import HeaderApp from "@/components/common/HeaderApp";
+import Dashboard from "@/pages/Alumno/Dahboard"
 
 export default function AppLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <HeaderApp />
+      <Dashboard/>
       <main className="flex-1 p-6">{children}</main>
     </div>
   );
