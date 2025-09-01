8372ee3642e19456d5bb7c7e67edd7810764b670
Alejandro <alejodev2022@gmail.com>
2025-09-01 10:23:35 -0500

    refactor: eliminar importaciones innecesarias y código comentado en el componente InstructoresCard para mejorar la legibilidad

--- Frontend/src/components/Landing/InstructoresCard.jsx
+++ Frontend/src/components/Landing/InstructoresCard.jsx
@@ -1,4 +1,4 @@
-import React, {  } from "react";
+import React from "react";
 import {
   Star,
   Users,
@@ -11,7 +11,6 @@ import {
 } from "lucide-react";
 
 export default function InstuctoresCard() {
-  
   //const [selectedInstructor, setSelectedInstructor] = useState(null);
 
   const instructorInfo = [
