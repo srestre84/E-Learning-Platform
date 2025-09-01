653d0555e4f65f6c677aa3558c9e989fb5120107
Alejandro <alejodev2022@gmail.com>
2025-09-01 10:26:07 -0500

    feat: actualizar la importación del componente LoginPage a AuthPage en publicRoutes, mejorando la organización de las rutas de autenticación.

--- Frontend/src/routes/publicRoutes.jsx
+++ Frontend/src/routes/publicRoutes.jsx
@@ -2,7 +2,7 @@ import LandingLayout from "@/components/layout/LandingLayout";
 import { lazy } from "react";
 
 const Home = lazy(() => import("@/pages/Home"));
-const LoginPage = lazy(() => import("@/pages/Login"));
+const LoginPage = lazy(() => import("@/pages/Auth/AuthPage"));
 const CatalogoCursos = lazy(() => import("@/pages/CatalogoCursos"));
 
 const publicRoutes = [
