d428bec03fd73b81ba868e3392fba351ae19ff79
Alejandro <alejodev2022@gmail.com>
2025-09-03 12:03:11 -0500

    refactor(App): mejorar la estructura del componente App.jsx, asegurando una mejor legibilidad y organización del código al ajustar la indentación y el formato.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,21 @@
-
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes/router";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { useState } from "react";
+
 function App() {
+ 
   return (
-      <AuthProvider>
-    <Suspense
-      fallback={
-        <div className="flex items-center justify-center h-screen">
-          Cargando...
-        </div>
-      }>
-      <RouterProvider router={router} />
-    </Suspense>
+    <AuthProvider>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-screen">
+            Cargando...
+          </div>
+        }>
+        <RouterProvider router={router}  />
+      </Suspense>
     </AuthProvider>
   );
 }
