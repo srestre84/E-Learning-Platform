2c8c2a327a8f90dfb33e8b154b12cba760299088
Alejandro <alejodev2022@gmail.com>
2025-09-05 17:05:34 -0500

    fix(useAuth): agregar manejo de error para uso incorrecto del contexto de autenticación

--- Frontend/src/contexts/ContextUse.jsx
+++ Frontend/src/contexts/ContextUse.jsx
@@ -1,6 +1,12 @@
 import { useContext } from "react";
-import AuthContext from "@/contexts/AuthContext"
+import { AuthContext } from "@/contexts/AuthContext";
 
 export function useAuth() {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error('useAuth debe ser usado dentro de un AuthProvider');
+  }
+
+  return context;
 }
