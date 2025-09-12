cc1fe7921b60b2ecd146bfe99d47f23069cada29
Juan-Valenzuela3 <juan_svalenzuela@soy.sena.edu.co>
2025-09-12 15:49:55 -0500

    CI/CD 1.0

--- Backend/Dev-learning-Platform/src/test/java/com/Dev_learning_Platform/Dev_learning_Platform/config/TestConfig.java
+++ Backend/Dev-learning-Platform/src/test/java/com/Dev_learning_Platform/Dev_learning_Platform/config/TestConfig.java
@@ -35,4 +35,5 @@ public class TestConfig {
     public AuthenticationDetailsProvider mockAuthenticationDetailsProvider() {
         return Mockito.mock(AuthenticationDetailsProvider.class);
     }
+
 }
