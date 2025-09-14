4ccf8a8743eedf3e83181df2e3fc0bc348db9a64
Juan-Valenzuela3 <juan_svalenzuela@soy.sena.edu.co>
2025-09-14 15:49:20 -0500

    Fix errors test

--- Backend/Dev-learning-Platform/src/main/java/com/Dev_learning_Platform/Dev_learning_Platform/services/CourseService.java
+++ Backend/Dev-learning-Platform/src/main/java/com/Dev_learning_Platform/Dev_learning_Platform/services/CourseService.java
@@ -3,6 +3,7 @@ package com.Dev_learning_Platform.Dev_learning_Platform.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +32,7 @@ public class CourseService {
     private final EnrollmentRepository enrollmentRepository;
 
     @Autowired
+    @Lazy
     private EnrollmentService enrollmentService;
 
 
