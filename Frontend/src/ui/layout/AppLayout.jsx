c27283712910e244e4cea8cb52a2565b92723de5
Alejandro <alejodev2022@gmail.com>
2025-09-18 01:13:43 -0500

    feat(AppLayout): actualizar importaciones y añadir HeaderApp
    - Se ha modificado la ruta de importación del componente Sidebar para utilizar un alias.
    - Se ha añadido el componente HeaderApp al layout principal, mejorando la estructura de la interfaz de usuario.

--- Frontend/src/ui/layout/AppLayout.jsx
+++ Frontend/src/ui/layout/AppLayout.jsx
@@ -1,8 +1,9 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import Sidebar from "../common/Sidebar";
+import Sidebar from "@/ui/common/Sidebar";
 import { useEffect } from "react";
 import { useAuth } from "@/contexts/useAuth";
 import { useSidebar } from "@/shared/hooks/useSidebar";
+import HeaderApp from "@/ui/common/HeaderApp";
 
 export default function AppLayout() {
   const navigate = useNavigate();
@@ -24,11 +25,13 @@ export default function AppLayout() {
       {/* Sidebar handles its own positioning and mobile behavior */}
       <Sidebar />
 
+
       {/* Main content */}
       <div
         className={`flex-1 flex flex-col overflow-hidden transition-all duration-300 ${
           isCollapsed ? "lg:ml-16" : "lg:ml-64"
         }`}>
+        <HeaderApp />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4">
           <div className="container mx-auto px-4 py-6">
             <Outlet />
