d135b29b5b72281800a05486a37913c908ccc295
Alejandro <alejodev2022@gmail.com>
2025-09-17 20:37:54 -0500

    refactor(RoutingGuards): estandarizar las importaciones y mejorar la legibilidad
    - Se han actualizado las importaciones en los componentes `PublicRoute` y `RoleBasedRedirect` para utilizar comillas dobles, mejorando la consistencia en el código.
    - Se ha optimizado la estructura del código, facilitando la lectura y comprensión de los componentes de enrutamiento.

--- Frontend/src/interfaces/routing/guards/PublicRoute.jsx
+++ Frontend/src/interfaces/routing/guards/PublicRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "@/shared/hooks/useAuth";
 
 const PublicRoute = ({ children }) => {
