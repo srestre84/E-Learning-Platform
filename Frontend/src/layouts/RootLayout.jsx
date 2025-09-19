7711546a50a12c3d0357af951e8c40016f3aad5b
Alejandro <alejodev2022@gmail.com>
2025-09-19 07:33:40 -0500

    Actualiza el componente RootLayout para mejorar la gestión de títulos de ruta. Se cambia el título por defecto a "Donde el aprendizaje es una aventura" y se actualiza el nombre de la aplicación a "EduPlatform", optimizando la presentación del layout.

--- Frontend/src/layouts/RootLayout.jsx
+++ Frontend/src/layouts/RootLayout.jsx
@@ -1,20 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import TitleManager from '@/shared/components/TitleManager';
-import { routeTitles } from '@/config/routeTitles';
+import { Outlet } from "react-router-dom";
+import TitleManager from "@/shared/components/TitleManager";
+import { routeTitles } from "@/config/routeTitles";
 
 const RootLayout = () => {
-    return (
-        <>
-
-            <TitleManager
-                routeTitles={routeTitles}
-                defaultTitle="| E-Learning Platform"
-                includeAppName={true}
-                appName="E-Learning Platform"
-            />
-            <Outlet />
-        </>
-    );
+  return (
+    <>
+      <TitleManager
+        routeTitles={routeTitles}
+        defaultTitle="Donde el aprendizaje es una aventura"
+        includeAppName={true}
+        appName="EduPlatform"
+      />
+      <Outlet />
+    </>
+  );
 };
 
 export default RootLayout;
